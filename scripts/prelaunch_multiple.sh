c63623ab2d4aa10e0b04b5174f637e1d02068cbf
Ruo Yu Tao <ruoyutao@gmail.com>
2025-02-14 11:46:06 -0500

    update prelaunch multiple for all navix runs

--- scripts/prelaunch_multiple.sh
+++ scripts/prelaunch_multiple.sh
@@ -1,4 +1,4 @@
-PRELAUNCH_PATHS='hyperparams/navix/navix_01_ppo.py hyperparams/navix/navix_01_ppo_memoryless.py hyperparams/navix/navix_01_ppo_observable.py hyperparams/navix/navix_02_ppo.py hyperparams/navix/navix_02_ppo_memoryless.py hyperparams/navix/navix_02_ppo_observable.py hyperparams/navix/navix_03_ppo.py hyperparams/navix/navix_03_ppo_memoryless.py hyperparams/navix/navix_03_ppo_observable.py'
+PRELAUNCH_PATHS='hyperparams/navix/navix_01_ppo.py hyperparams/navix/navix_01_ppo_memoryless.py hyperparams/navix/navix_01_ppo_LD.py hyperparams/navix/navix_02_ppo.py hyperparams/navix/navix_02_ppo_memoryless.py hyperparams/navix/navix_02_ppo_LD.py hyperparams/navix/navix_03_ppo.py hyperparams/navix/navix_03_ppo_memoryless.py hyperparams/navix/navix_03_ppo_LD.py'
 
 for item in $PRELAUNCH_PATHS;
 do
