72af57f343106f348d24be637936a380922be5ab
Ruo Yu Tao <ruoyutao@gmail.com>
2025-02-14 08:02:08 -0500

    exclude gpu 2106

--- scripts/launching/onager_slurm_launch.sh
+++ scripts/launching/onager_slurm_launch.sh
@@ -1,6 +1,6 @@
 cd ../../
 
-PRELAUNCH_NAMES='masked_mujoco_ppo masked_mujoco_ppo_memoryless masked_mujoco_ppo_observable'
+PRELAUNCH_NAMES='navix_01_ppo navix_01_ppo_memoryless navix_01_ppo_observable'
 
 for item in $PRELAUNCH_NAMES;
 do
@@ -9,9 +9,10 @@ do
       --jobname "$item" \
       --mem 24 \
       --cpus 3 \
-      --duration 2-00:00:00 \
+      --duration 0-12:00:00 \
       --venv venv \
       --gpus 1 \
       --partition 3090-gcondo \
+      -q gpu2106 \
       # --tasks-per-node 5
 done
