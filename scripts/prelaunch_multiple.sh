50bf83bd239a46b6a9c91be91d0612a80e628dd0
Ruo Yu Tao <ruoyutao@gmail.com>
2025-02-15 12:59:48 -0500

    now do it for 15_15

--- scripts/prelaunch_multiple.sh
+++ scripts/prelaunch_multiple.sh
@@ -1,4 +1,4 @@
-PRELAUNCH_PATHS='rocksample/rocksample_11_11_ppo.py rocksample/rocksample_11_11_ppo_LD.py rocksample/rocksample_11_11_ppo_memoryless.py rocksample/rocksample_11_11_ppo_perfect_memory_memoryless.py rocksample/rocksample_11_11_ppo_perfect_memory.py'
+PRELAUNCH_PATHS='rocksample/rocksample_15_15_ppo.py rocksample/rocksample_15_15_ppo_LD.py rocksample/rocksample_15_15_ppo_memoryless.py rocksample/rocksample_15_15_ppo_perfect_memory_memoryless.py rocksample/rocksample_15_15_ppo_perfect_memory.py'
 
 for item in $PRELAUNCH_PATHS;
 do
