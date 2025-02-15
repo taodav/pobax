9ec3ebc1d9802c33bb05d54c1cfbda227e2fd586
Ruo Yu Tao <ruoyutao@gmail.com>
2025-02-15 11:44:48 -0500

    break up navix_02_ppo_LD runs, add all pocman runs

--- scripts/hyperparams/pocman/pocman_perfect_memory_memoryless_ppo.py
+++ scripts/hyperparams/pocman/pocman_perfect_memory_memoryless_ppo.py
@@ -3,7 +3,7 @@ from pathlib import Path
 exp_name = Path(__file__).stem
 
 lrs = [2.5e-3, 2.5e-4, 2.5e-5, 2.5e-6]
-lambda0s = [0.95]
+lambda0s = [0.1, 0.5, 0.7, 0.9, 0.95]
 lambda1s = [0.95]
 alphas = [1]
 ld_weights = [0]
@@ -20,7 +20,7 @@ hparams = {
             'perfect_memory': True,
             'memoryless': True,
             'lr': lrs,
-            'lambda0': ' '.join(map(str, lambda0s)),
+            'lambda0': lambda0s,
             'lambda1': ' '.join(map(str, lambda1s)),
             'alpha': ' '.join(map(str, alphas)),
             'ld_weight': ' '.join(map(str, ld_weights)),
--- scripts/parse_multiple_results.sh
+++ scripts/parse_multiple_results.sh
@@ -1,7 +1,7 @@
-RESULTS_PATHS='../results/navix_01_ppo_memoryless'
+RESULTS_PATHS='navix_01_ppo navix_01_ppo_LD navix_01_ppo_memoryless navix_02_ppo navix_02_ppo_LD navix_03_ppo_memoryless navix_03_ppo_LD navix_03_ppo_memoryless'
 
 for item in $RESULTS_PATHS;
 do
-  python parse_batch_experiments.py --discounted "$item"
-  python best_hyperparams_per_env.py "$item/parsed_hparam_scores.pkl"
+  python parse_batch_experiments.py --discounted "../results/$item"
+  python best_hyperparams_per_env.py "../results/$item/parsed_hparam_scores.pkl"
 done
