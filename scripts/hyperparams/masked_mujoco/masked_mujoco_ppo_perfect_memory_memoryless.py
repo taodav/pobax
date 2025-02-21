12e27137f77d438783ab8fe7e5bf5a6fff1122a6
Ruo Yu Tao <ruoyutao@gmail.com>
2025-02-21 12:45:17 -0500

    split up masked_mujoco runs

--- scripts/hyperparams/masked_mujoco/masked_mujoco_ppo_perfect_memory_memoryless.py
+++ scripts/hyperparams/masked_mujoco/masked_mujoco_ppo_perfect_memory_memoryless.py
@@ -4,7 +4,7 @@ import importlib
 exp_name = Path(__file__).stem
 
 lrs = [2.5e-3, 2.5e-4, 2.5e-5, 2.5e-6]
-lambda0s = [0.95]
+lambda0s = [0.1, 0.3, 0.5, 0.7, 0.9, 0.95]
 lambda1s = [0.95]
 alphas = [1]
 ld_weights = [0]
