0e39644426d93115ab3083d553394475a4908d9d
kaicheng_guo@brown.edu <kguo32@vscode1.oscar.ccv.brown.edu>
2025-02-21 21:12:55 -0500

    update rocksample

--- scripts/hyperparams/rocksample/rocksample_11_11_transformer.py
+++ scripts/hyperparams/rocksample/rocksample_11_11_transformer.py
@@ -3,7 +3,7 @@ from pathlib import Path
 exp_name = Path(__file__).stem
 
 lrs = [2.5e-3, 2.5e-4, 2.5e-5, 2.5e-6]
-lambda0s = [0.1, 0.5, 0.7, 0.9, 0.95]
+lambda0s = [0.1, 0.3, 0.5, 0.7, 0.9, 0.95]
 lambda1s = [0.95]
 alphas = [1]
 ld_weights = [0]
@@ -11,25 +11,27 @@ ld_weights = [0]
 hparams = {
     'file_name':
         f'runs_{exp_name}.txt',
-    'entry': '-m pobax.algos.ppo',
+    'entry': '-m pobax.algos.transformer_xl',
     'args': [
         {
-            'env': 'rocksample_15_15',
+            'env': 'rocksample_11_11',
             'double_critic': False,
             'action_concat': True,
-            'lr': ' '.join(map(str, lrs)),
+            'lr': lrs,
             'lambda0': lambda0s,
             'lambda1': ' '.join(map(str, lambda1s)),
             'alpha': ' '.join(map(str, alphas)),
             'ld_weight': ' '.join(map(str, ld_weights)),
-            'hidden_size': 512,
+            'hidden_size': 256,
+            'embed_size': 96,
+            'num_envs': 8,
             'entropy_coeff': 0.2,
-            'num_envs': 32,
-            'steps_log_freq': 8,
-            'update_log_freq': 10,
-            'total_steps': int(1e7),
-            'seed': 2025,
+            'steps_log_freq': 4,
+            'update_log_freq': 5,
+            'total_steps': int(5e6),
+            'seed': 2024,
             'n_seeds': 5,
+            'debug': True,
             'platform': 'gpu',
             'study_name': exp_name
         }
