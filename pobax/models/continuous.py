211d70f1052d7ffce5d5b0da8c7760ed8748af89
KevinGuo27 <113325820+KevinGuo27@users.noreply.github.com>
2025-02-24 16:26:51 -0500

    update continuous transformer

--- pobax/models/continuous.py
+++ pobax/models/continuous.py
@@ -178,85 +178,48 @@ class ContinuousActorCriticTransformer(nn.Module):
     gating_bias:float=0.
     activation: str = "tanh"
     double_critic: bool = False
-    
-    @nn.compact
-    def __call__(self, memories,obs,mask):
-        transformer = Transformer(
-                                encoder_size=self.encoder_size,
-                                num_heads=self.num_heads,
-                                qkv_features=self.qkv_features,
-                                num_layers=self.num_layers,gating=self.gating,gating_bias=self.gating_bias)
-        embedding, memory_out = transformer(memories,obs,mask)
-        actor = ContinuousActor(self.action_dim, hidden_size=self.hidden_size,
+
+    def setup(self):
+        self.transformer = Transformer(
+            encoder_size=self.encoder_size,
+            num_heads=self.num_heads,
+            qkv_features=self.qkv_features,
+            num_layers=self.num_layers,
+            gating=self.gating,
+            gating_bias=self.gating_bias
+        )
+        self.actor = ContinuousActor(self.action_dim, hidden_size=self.hidden_size,
                                 activation=self.activation)
-        pi = actor(embedding)
-
-        critic = Critic(hidden_size=self.hidden_size)
-
         if self.double_critic:
-            critic = nn.vmap(Critic,
+            self.critic = nn.vmap(Critic,
                              variable_axes={'params': 0},
                              split_rngs={'params': True},
                              in_axes=None,
                              out_axes=2,
                              axis_size=2)(hidden_size=self.hidden_size)
-
-        v = critic(embedding)
+        else:
+            self.critic = Critic(hidden_size=self.hidden_size)
+    
+    def __call__(self, memories,obs,mask):
+        embedding, memory_out = self.transformer(memories,obs,mask)
+        pi = self.actor(embedding)
+        v = self.critic(embedding)
         return pi, jnp.squeeze(v, axis=-1),memory_out
     
-    @nn.compact
     def model_forward_eval(self, memories,obs,mask):
         """Used during environment rollout (single timestep of obs). And return the memory"""
-        transformer = Transformer(
-                                encoder_size=self.encoder_size,
-                                num_heads=self.num_heads,
-                                qkv_features=self.qkv_features,
-                                num_layers=self.num_layers,gating=self.gating,gating_bias=self.gating_bias)
-        embedding,memory_out = transformer.forward_eval(memories,obs,mask)
+        embedding,memory_out = self.transformer.forward_eval(memories,obs,mask)
+        pi = self.actor(embedding)
 
-        actor = ContinuousActor(self.action_dim, hidden_size=self.hidden_size,
-                                activation=self.activation)
-        pi = actor(embedding)
-
-        critic = Critic(hidden_size=self.hidden_size)
-
-        if self.double_critic:
-            critic = nn.vmap(Critic,
-                             variable_axes={'params': 0},
-                             split_rngs={'params': True},
-                             in_axes=None,
-                             out_axes=2,
-                             axis_size=2)(hidden_size=self.hidden_size)
-
-        v = critic(embedding)
+        v = self.critic(embedding)
 
         return pi, jnp.squeeze(v, axis=-1),memory_out
     
-    @nn.compact
     def model_forward_train(self, memories,obs,mask): 
         """Used during training: a window of observation is sent. And don't return the memory"""
-        transformer = Transformer(
-                                encoder_size=self.encoder_size,
-                                num_heads=self.num_heads,
-                                qkv_features=self.qkv_features,
-                                num_layers=self.num_layers,gating=self.gating,gating_bias=self.gating_bias)
-        embedding = transformer.forward_train(memories,obs,mask)
-
-        actor = ContinuousActor(self.action_dim, hidden_size=self.hidden_size,
-                                activation=self.activation)
-        pi = actor(embedding)
-
-        critic = Critic(hidden_size=self.hidden_size)
-
-        if self.double_critic:
-            critic = nn.vmap(Critic,
-                             variable_axes={'params': 0},
-                             split_rngs={'params': True},
-                             in_axes=None,
-                             out_axes=2,
-                             axis_size=2)(hidden_size=self.hidden_size)
-
-        v = critic(embedding)
+        embedding = self.transformer.forward_train(memories,obs,mask)
+        pi = self.actor(embedding)
+        v = self.critic(embedding)
         return pi, jnp.squeeze(v, axis=-1)
     
 
@@ -271,106 +234,68 @@ class ImageContinuousActorCriticTransformer(nn.Module):
     gating:bool=False
     gating_bias:float=0.
     double_critic: bool = False
-    
-    @nn.compact
-    def __call__(self, memories,obs,mask):
-        if obs.shape[-2] >= 20:
-            embedding = FullImageCNN(hidden_size=self.hidden_size)(obs)
-        else:
-            embedding = SmallImageCNN(hidden_size=self.hidden_size)(obs)
-        embedding = nn.relu(embedding)
-        embedding = embedding.squeeze(1)
 
-        transformer = Transformer(
-                                encoder_size=self.encoder_size,
-                                num_heads=self.num_heads,
-                                qkv_features=self.qkv_features,
-                                num_layers=self.num_layers,gating=self.gating,gating_bias=self.gating_bias)
-        
-        embedding, memory_out = transformer(memories,embedding,mask)
-        actor = ContinuousActor(self.action_dim, hidden_size=self.hidden_size,
+    def setup(self):
+        self.cnn_full = FullImageCNN(hidden_size=self.hidden_size)
+        self.cnn_small = SmallImageCNN(hidden_size=self.hidden_size)
+        self.transformer = Transformer(
+            encoder_size=self.encoder_size,
+            num_heads=self.num_heads,
+            qkv_features=self.qkv_features,
+            num_layers=self.num_layers,
+            gating=self.gating,
+            gating_bias=self.gating_bias
+        )
+        self.actor = ContinuousActor(self.action_dim, hidden_size=self.hidden_size,
                                 activation=self.activation)
-        pi = actor(embedding)
-
-        critic = Critic(hidden_size=self.hidden_size)
-
         if self.double_critic:
-            critic = nn.vmap(Critic,
+            self.critic = nn.vmap(Critic,
                              variable_axes={'params': 0},
                              split_rngs={'params': True},
                              in_axes=None,
                              out_axes=2,
                              axis_size=2)(hidden_size=self.hidden_size)
-
-        v = critic(embedding)
+        else:
+            self.critic = Critic(hidden_size=self.hidden_size)
+    
+    def __call__(self, memories,obs,mask):
+        if obs.shape[-2] >= 20:
+            embedding = self.cnn_full(obs)
+        else:
+            embedding = self.cnn_small(obs)
+        embedding = nn.relu(embedding)
+        
+        embedding, memory_out = self.transformer(memories,embedding,mask)
+        pi = self.actor(embedding)
+        v = self.critic(embedding)
         return pi, jnp.squeeze(v, axis=-1),memory_out
     
-    @nn.compact
     def model_forward_eval(self, memories,obs,mask):
         """Used during environment rollout (single timestep of obs). And return the memory"""
         if obs.shape[-2] >= 20:
-            embedding = FullImageCNN(hidden_size=self.hidden_size)(obs)
+            embedding = self.cnn_full(obs)
         else:
-            embedding = SmallImageCNN(hidden_size=self.hidden_size)(obs)
+            embedding = self.cnn_small(obs)
         embedding = nn.relu(embedding)
-        embedding = embedding.squeeze(1)
-
-        transformer = Transformer(
-                                encoder_size=self.encoder_size,
-                                num_heads=self.num_heads,
-                                qkv_features=self.qkv_features,
-                                num_layers=self.num_layers,gating=self.gating,gating_bias=self.gating_bias)
         
-        embedding,memory_out = transformer.forward_eval(memories,embedding,mask)
+        embedding,memory_out = self.transformer.forward_eval(memories,embedding,mask)
+        pi = self.actor(embedding)
 
-        actor = ContinuousActor(self.action_dim, hidden_size=self.hidden_size,
-                                activation=self.activation)
-        pi = actor(embedding)
-
-        critic = Critic(hidden_size=self.hidden_size)
-
-        if self.double_critic:
-            critic = nn.vmap(Critic,
-                             variable_axes={'params': 0},
-                             split_rngs={'params': True},
-                             in_axes=None,
-                             out_axes=2,
-                             axis_size=2)(hidden_size=self.hidden_size)
-
-        v = critic(embedding)
+        v = self.critic(embedding)
 
         return pi, jnp.squeeze(v, axis=-1),memory_out
     
-    @nn.compact
     def model_forward_train(self, memories,obs,mask): 
         """Used during training: a window of observation is sent. And don't return the memory"""
         if obs.shape[-2] >= 20:
-            embedding = FullImageCNN(hidden_size=self.hidden_size)(obs)
+            embedding = self.cnn_full(obs)
         else:
-            embedding = SmallImageCNN(hidden_size=self.hidden_size)(obs)
+            embedding = self.cnn_small(obs)
         embedding = nn.relu(embedding)
-
-        transformer = Transformer(
-                                encoder_size=self.encoder_size,
-                                num_heads=self.num_heads,
-                                qkv_features=self.qkv_features,
-                                num_layers=self.num_layers,gating=self.gating,gating_bias=self.gating_bias)
         
-        embedding = transformer.forward_train(memories,embedding,mask)
+        embedding = self.transformer.forward_train(memories,embedding,mask)
 
-        actor = ContinuousActor(self.action_dim, hidden_size=self.hidden_size,
-                                activation=self.activation)
-        pi = actor(embedding)
+        pi = self.actor(embedding)
 
-        critic = Critic(hidden_size=self.hidden_size)
-
-        if self.double_critic:
-            critic = nn.vmap(Critic,
-                             variable_axes={'params': 0},
-                             split_rngs={'params': True},
-                             in_axes=None,
-                             out_axes=2,
-                             axis_size=2)(hidden_size=self.hidden_size)
-
-        v = critic(embedding)
+        v = self.critic(embedding)
         return pi, jnp.squeeze(v, axis=-1)
