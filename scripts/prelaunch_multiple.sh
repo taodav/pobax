6791eb2bf729479c49beb7845639110577d80f49
Ruo Yu Tao <ruoyutao@gmail.com>
2025-02-24 17:46:10 -0500

    add navix 01ablation runs

--- scripts/prelaunch_multiple.sh
+++ scripts/prelaunch_multiple.sh
@@ -1,4 +1,5 @@
-PRELAUNCH_PATHS='rocksample/rocksample_15_15_ppo.py rocksample/rocksample_15_15_ppo_LD.py rocksample/rocksample_15_15_ppo_memoryless.py rocksample/rocksample_15_15_ppo_perfect_memory_memoryless.py rocksample/rocksample_15_15_ppo_perfect_memory.py'
+#PRELAUNCH_PATHS='rocksample/rocksample_15_15_ppo.py rocksample/rocksample_15_15_ppo_LD.py rocksample/rocksample_15_15_ppo_memoryless.py rocksample/rocksample_15_15_ppo_perfect_memory_memoryless.py rocksample/rocksample_15_15_ppo_perfect_memory.py'
+PRELAUNCH_PATHS='navix/ablation/navix_01_ppo_hsize_sweep.py navix/ablation/navix_01_ppo_memoryless_hsize_sweep.py navix/ablation/navix_01_ppo_perfect_memory_hsize_sweep.py'
 
 for item in $PRELAUNCH_PATHS;
 do
