a5669fb96d2524e3743a101c5f221eb5e2c2c6e6
Ruo Yu Tao <ruoyutao@gmail.com>
2025-02-24 23:34:10 -0500

    split nenvs sweep again...

--- scripts/hyperparams/navix/ablation/navix_01_ppo_perfect_memory_nenvs_sweep.py
+++ scripts/hyperparams/navix/ablation/navix_01_ppo_perfect_memory_nenvs_sweep.py
@@ -18,7 +18,7 @@ hparams = {
             'double_critic': False,
             'memoryless': False,
             'action_concat': True,
-            'lr': ' '.join(map(str, lrs)),
+            'lr': lrs,
             'anneal_lr': True,
             'hidden_size': 256,
             'lambda0': lambda0s,
