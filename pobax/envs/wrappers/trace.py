3c2ff2bca5259e27cd6823629fc8d3aca7bdb125
Ruo Yu Tao <ruoyutao@gmail.com>
2025-04-19 20:34:44 -0400

    add tested trace

--- pobax/envs/wrappers/trace.py
+++ pobax/envs/wrappers/trace.py
@@ -4,7 +4,7 @@ from typing import Optional, Tuple, Union
 import chex
 from flax import struct
 import jax
-from jax.numpy import jnp
+import jax.numpy as jnp
 from gymnax.environments import environment, spaces
 
 from pobax.envs.wrappers.gymnax import GymnaxWrapper
@@ -19,7 +19,8 @@ class TraceFeatureState:
 class TraceFeaturesWrapper(GymnaxWrapper):
     def __init__(self,
                  env: environment.Environment,
-                 lambdas: jnp.ndarray = jnp.array([0.1, 0.9, 0.95]),
+                 lambdas: jnp.ndarray = jnp.array([0., 0.5, 0.7, 0.9, 0.95]),
+                 trace_in_obs: bool = False,
                  **kwargs):
         """
         TraceFeaturesWrapper adds trace features over observations for each lambda.
@@ -30,29 +31,41 @@ class TraceFeaturesWrapper(GymnaxWrapper):
         """
         super().__init__(env)
         self.lambdas = lambdas
+        self.trace_in_obs = trace_in_obs
 
-    def action_size(self, params):
-        action_space = self.action_space(params)
-        if isinstance(action_space, spaces.Box):
-            assert len(action_space.shape) == 1
-            action_size = action_space.shape[0]
-        elif isinstance(action_space, spaces.Discrete):
-            action_size = action_space.n
+    def observation_space(self, params) -> spaces.Box:
+        og_obs_space_shape = self._env.observation_space(params).shape
+        if self.trace_in_obs:
+            n_lambdas = len(self.lambdas)
+
+            if len(og_obs_space_shape) == 1:
+                obs_length = og_obs_space_shape[0]
+                shape = obs_length * n_lambdas
+            elif len(og_obs_space_shape) == 3:
+                # images
+                shape = (og_obs_space_shape[0], og_obs_space_shape[1], og_obs_space_shape[2] * n_lambdas)
+            else:
+                raise NotImplementedError
+
+            return spaces.Box(
+                low=self._env.observation_space(params).low,
+                high=self._env.observation_space(params).high,
+                shape=shape,
+                dtype=self._env.observation_space(params).dtype,
+            )
         else:
-            raise NotImplementedError
-        return action_size
+            return self._env.observation_space(params)
 
-    @partial(jax.jit, static_argnums=(0,-1))
+    @partial(jax.jit, static_argnums=(0, -1))
     def reset(
             self, key: chex.PRNGKey, params: Optional[environment.EnvParams] = None
     ) -> Tuple[chex.Array, TraceFeatureState]:
         obs, state = self._env.reset(key, params)
-        action_vec = jnp.zeros(self.action_size(params))
 
-        obs_action = jnp.concatenate((obs, action_vec))
+        trace_features = obs[..., None].repeat(len(self.lambdas), axis=-1)
 
         state = TraceFeatureState(env_state=state,
-                                  trace_features=obs_action[..., None])
+                                  trace_features=trace_features)
 
         return obs, state
 
@@ -66,19 +79,13 @@ class TraceFeaturesWrapper(GymnaxWrapper):
     ) -> Tuple[chex.Array, TraceFeatureState, float, bool, dict]:
         obs, next_state, reward, done, info = self._env.step(key, state, action, params)
 
-        action_vec = action
-        action_space = self.action_space(params)
-        if isinstance(action_space, spaces.Discrete):
-            action_vec = jnp.eye(action_space.n)[action]
-
-        obs_action = jnp.concatenate((obs, action_vec))
-
         leading_dims = (1,) * len(obs.shape)
         lambdas = jnp.broadcast_to(self.lambdas, leading_dims + self.lambdas.shape)
 
-        next_trace = (1 - done) * lambdas * state.trace_features + obs_action[..., None]
+        next_trace = (1 - done) * lambdas * state.trace_features + (1 - lambdas) * obs[..., None]
 
         next_state = TraceFeatureState(env_state=next_state,
                                        trace_features=next_trace)
 
         return obs, next_state, reward, done, info
+
