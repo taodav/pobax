09eb622c9bdb5d2026e3ef3ff176aba2b0e43fb5
Ruo Yu Tao <ruoyutao@gmail.com>
2025-04-26 20:48:03 -0400

    rocksample sf obs

--- pobax/algos/gd_ppo.py
+++ pobax/algos/gd_ppo.py
@@ -551,7 +551,7 @@ def make_train(args: GDPPOHyperparams, rand_key: jax.random.PRNGKey):
 
 
 if __name__ == "__main__":
-    jax.disable_jit(True)
+    # jax.disable_jit(True)
     # okay some weirdness here. NUM_ENVS needs to match with NUM_MINIBATCHES
     args = GDPPOHyperparams().parse_args()
     jax.config.update('jax_platform_name', args.platform)
