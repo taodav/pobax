5949395aebf9391c9bed8487a829c3fe2e0bff75
Ruo Yu Tao <ruoyutao@gmail.com>
2025-05-08 15:30:13 -0400

    add navix runs for obs and hiidden state diffs

---
+++ scripts/hyperparams/gd_sf/navix/navix_01_gd_sf_ppo_hs_diff_discrep.py
@@ -0,0 +1,43 @@
+from pathlib import Path
+
+exp_name = Path(__file__).stem
+
+lrs = [2.5e-3, 2.5e-4, 2.5e-5]
+lambda0s = [0.1, 0.95]
+lambda1s = [0.5, 0.7, 0.95]
+alphas = [1]
+ld_weights = [0., 0.25, 0.5]
+vf_coeffs = [0.5]  # Value function coefficients
+
+hparams = {
+    'file_name':
+        f'runs_{exp_name}.txt',
+    'entry': '-m pobax.algos.gd_ppo',
+    'args': [
+        {
+            'env': 'Navix-DMLab-Maze-01-v0',
+            'double_critic': True,
+            'memoryless': False,
+            'action_concat': True,
+            'lr': lrs,
+            'anneal_lr': True,
+            'hidden_size': 256,
+            'lambda0': ' '.join(map(str, lambda0s)),
+            'lambda1': lambda1s,
+            'alpha': ' '.join(map(str, alphas)),
+            'ld_weight': ld_weights,
+            'entropy_coeff': 0.02,
+            'num_steps': 128,
+            'num_envs': 32,
+            'cumulant_type': 'hs_diff',
+            'cumulant_loss_weight': 0.25,
+            'steps_log_freq': 8,
+            'update_log_freq': 5,
+            'total_steps': int(1e7),
+            'seed': 2025,
+            'n_seeds': 5,
+            'platform': 'gpu',
+            'study_name': exp_name
+        }
+    ]
+}
