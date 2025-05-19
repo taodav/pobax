15b51db9ef9f61061ceb945630a535495dd47058
Ruo Yu Tao <ruoyutao@gmail.com>
2025-05-19 14:01:39 -0700

    add discrep runs for sweep

--- scripts/hyperparams/gd_sf/rocksample/rocksample_11_11_ppo_gd_sf_sweep_discrep.py
+++ scripts/hyperparams/gd_sf/rocksample/rocksample_11_11_ppo_gd_sf_sweep_discrep.py
@@ -5,7 +5,7 @@ exp_name = Path(__file__).stem
 lrs = [2.5e-3, 2.5e-4, 2.5e-5, 2.5e-6]  # Learning rates
 lambda0s = [0.1, 0.3, 0.5, 0.7, 0.95]
 lambda1s = [0.1, 0.3, 0.5, 0.7, 0.95]
-alphas = [1]
+cumulant_loss_weights = [0.1, 0.25, 0.5, 0.75]
 ld_weights = [0., 0.25, 0.5]
 vf_coeffs = [0.5]  # Value function coefficients
 
@@ -21,15 +21,16 @@ hparams = {
             'reward_concat': False,
             'lr': ' '.join(map(str, lrs)),
             'lambda0': ' '.join(map(str, lambda0s)),
-            'lambda1': lambda1s,
-            'alpha': ' '.join(map(str, alphas)),
-            'ld_weight': ld_weights,
+            'lambda1': ' '.join(map(str, lambda1s)),
+            'ld_weight': ' '.join(map(str, ld_weights)),
             'hidden_size': 256,
             'num_envs': 8,
-            'cumulant_type': 'hs',
-            'cumulant_loss_weight': 0.5,
-            'cumulant_diff': True,
+            'cumulant_type': ['hs', 'obs', 'enc_obs'],
+            'cumulant_transform': ['none', 'random_proj'],
+            'cumulant_loss_weight': ' '.join(map(str, cumulant_loss_weights)),
             'add_reward_to_cumulant': True,
+            'sweep_type': 'random',
+            'n_random_hparams': 30,
             'entropy_coeff': 0.2,
             'steps_log_freq': 4,
             'update_log_freq': 5,
