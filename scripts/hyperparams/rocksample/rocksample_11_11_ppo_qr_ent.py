6bdda3684bfadf34f978f9d3a821eb5a65afb2b0
Ruo Yu Tao <ruoyutao@gmail.com>
2025-06-09 10:11:45 -0700

    final runs

--- scripts/hyperparams/rocksample/rocksample_11_11_ppo_qr_ent.py
+++ scripts/hyperparams/rocksample/rocksample_11_11_ppo_qr_ent.py
@@ -2,10 +2,12 @@ from pathlib import Path
 
 exp_name = Path(__file__).stem
 
-lrs = [2.5e-2, 2.5e-3, 2.5e-4, 2.5e-5, 2.5e-6]
-lambda0s = [0., 0.1, 0.3, 0.5, 0.7, 0.9, 0.95]
+lrs = [2.5e-3, 2.5e-4, 2.5e-5, 2.5e-6]  # Learning rates
+lambda0s = [0.1, 0.3, 0.5, 0.7, 0.9, 0.95]
 lambda1s = [0.95]
-ld_weights = [0]
+quantile_entropy_coeffs = [0.1, 0.25, 0.5, 0.75]
+ld_weights = [0.]
+vf_coeffs = [0.5]  # Value function coefficients
 
 hparams = {
     'file_name':
@@ -20,6 +22,7 @@ hparams = {
             'lambda0': ' '.join(map(str, lambda0s)),
             'lambda1': ' '.join(map(str, lambda1s)),
             'ld_weight': ' '.join(map(str, ld_weights)),
+            'quantile_entropy_coeff': ' '.join(map(str, quantile_entropy_coeffs)),
             'hidden_size': 256,
             'num_envs': 16,
             'entropy_coeff': 0.05,
