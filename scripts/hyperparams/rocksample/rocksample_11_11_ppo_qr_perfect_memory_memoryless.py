d13925270b8ebec5a5b4e5a5e94c246fcf6b7880
Ruo Yu Tao <ruoyutao@gmail.com>
2025-06-11 10:51:35 -0700

    add memoryless runs

---
+++ scripts/hyperparams/rocksample/rocksample_11_11_ppo_qr_perfect_memory_memoryless.py
@@ -0,0 +1,39 @@
+from pathlib import Path
+
+exp_name = Path(__file__).stem
+
+lrs = [2.5e-2, 2.5e-3, 2.5e-4, 2.5e-5, 2.5e-6]
+lambda0s = [0., 0.1, 0.3, 0.5, 0.7, 0.9, 0.95]
+lambda1s = [0.95]
+ld_weights = [0]
+
+hparams = {
+    'file_name':
+        f'runs_{exp_name}.txt',
+    'entry': '-m pobax.algos.qr_ppo',
+    'args': [
+        {
+            'env': 'rocksample_11_11',
+            'memoryless': True,
+            'perfect_memory': True,
+            'double_critic': False,
+            'action_concat': True,
+            'lr': ' '.join(map(str, lrs)),
+            'lambda0': ' '.join(map(str, lambda0s)),
+            'lambda1': ' '.join(map(str, lambda1s)),
+            'ld_weight': ' '.join(map(str, ld_weights)),
+            'hidden_size': 256,
+            'num_envs': 32,
+            'entropy_coeff': 0.05,
+            'sweep_type': 'random',
+            'n_random_hparams': 30,
+            'steps_log_freq': 8,
+            'update_log_freq': 10,
+            'total_steps': int(5e6),
+            'seed': 2024,
+            'n_seeds': 5,
+            'platform': 'gpu',
+            'study_name': exp_name
+        }
+    ]
+}
