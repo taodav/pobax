f6eed5bbb9016ab2f779e62893a9469a2f55406a
Ruo Yu Tao <ruoyutao@gmail.com>
2025-09-05 13:24:26 -0400

    refactor skip

---
+++ pobax/models/skip.py
@@ -0,0 +1,78 @@
+from functools import partial
+
+import jax
+from flax import linen as nn
+from flax import struct
+from jax import numpy as jnp
+
+
+@struct.dataclass
+class SkipHiddenState:
+    hidden_state: jnp.ndarray
+    u_bar: jnp.ndarray
+
+def round_ste(x: jnp.ndarray) -> jnp.ndarray:
+    # forward = round(x); backward d/dx = 1 (passes gradient through)
+    return x + jax.lax.stop_gradient(jnp.round(x) - x)
+
+
+class SkipRNN(nn.Module):
+    """
+    RNN with a gated skip connection.
+    """
+    hidden_size: int
+    out_size: int
+    binarize_type: str = 'round'  # bernoulli | round
+
+    @partial(
+        nn.scan,
+        variable_broadcast="params",
+        in_axes=0,
+        out_axes=0,
+        split_rngs={"params": False},
+    )
+    @nn.compact
+    def __call__(self, carry: SkipHiddenState, key: jax.random.PRNGKey, x: jnp.ndarray):
+        """Applies the module."""
+        state = carry
+        ins, resets = x
+        reset_hs = self.initialize_carry(ins.shape[0], self.hidden_size)
+        state = jax.tree.map(lambda reset_hs, hs: jnp.where(
+            resets[:, jnp.newaxis],
+            reset_hs,
+            hs,
+        ), reset_hs, state)
+
+        # binarize in the actor loop. Need to do the u prediction here.
+        # Write a small test for this
+        u_bar = state.u_bar
+        if self.binarize_type == 'bernoulli':
+            u = jax.random.bernoulli(key, p=u_bar).astype(ins.dtype)  # (B, 1) in {0,1}
+        elif self.binarize_type == 'round':
+            u = round_ste(u_bar).astype(ins.dtype)
+        else:
+            raise NotImplementedError
+
+        new_hidden_state, embedding = nn.GRUCell(features=self.hidden_size)(state.hidden_state, ins)
+        new_hidden_state = u * state.hidden_state + (1 - u) * new_hidden_state
+
+        # The next part is taken from the SkipRNN paper.
+        # TODO: do alternative versions of this? maybe we do want our u to be recurrent.
+        change_in_u_logit = nn.Dense(features=1)(new_hidden_state)
+        change_in_u = jax.nn.sigmoid(change_in_u_logit)
+        next_ubar = u * change_in_u + (1 - u) * (u_bar + jnp.minimum(change_in_u_logit, 1 - u_bar))
+
+        logits = nn.Dense(features=self.out_size)(embedding)
+
+        # TODO: change this for general predictions
+        out = nn.sigmoid(logits)
+
+        return SkipHiddenState(hidden_state=new_hidden_state, u_bar=next_ubar), (out, logits, u, u_bar)
+
+    @staticmethod
+    def initialize_carry(batch_size, hidden_size) -> SkipHiddenState:
+        new_hs = nn.GRUCell(features=hidden_size).initialize_carry(
+            jax.random.PRNGKey(0), (batch_size, hidden_size)
+        ).astype(float)
+        new_u = jnp.zeros((batch_size, 1)).astype(float)
+        return SkipHiddenState(new_hs, new_u)
--- scripts/skip_rnn_tests.py
+++ scripts/skip_rnn_tests.py
@@ -1,8 +1,6 @@
 from functools import partial
 from time import time
 
-from flax import struct
-import flax.linen as nn
 from flax.training.train_state import TrainState
 import jax
 import jax.numpy as jnp
@@ -12,6 +10,8 @@ import optax
 # ------------------------------------------------------------
 from jax import random
 from typing import Dict, Callable
+
+from pobax.models.skip import SkipRNN
 
 
 def _batch(sample_fn, key, batch_size, **kwargs) -> Dict[str, jnp.ndarray]:
@@ -33,73 +33,14 @@ def sample_pulse_count(key, T=200, p=0.08):
 def make_pulse_count_batch(key, batch_size=32, T=200, p=0.08):
     return _batch(sample_pulse_count, key, batch_size, T=T, p=p)
 
-@struct.dataclass
-class SkipHiddenState:
-    hidden_state: jnp.ndarray
-    u_bar: jnp.ndarray
 
 def bernoulli_logprob_from_logits(b, a):
     # a: logits, b in {0,1}
     # log p(b=1) = log_sigmoid(a); log p(b=0) = log_sigmoid(-a)
     return b * jax.nn.log_sigmoid(a) + (1.0 - b) * jax.nn.log_sigmoid(-a)
 
-class SkipRNN(nn.Module):
-    """
-    RNN with a gated skip connection.
-    """
-    hidden_size: int
-    out_size: int
 
-    @partial(
-        nn.scan,
-        variable_broadcast="params",
-        in_axes=0,
-        out_axes=0,
-        split_rngs={"params": False},
-    )
-    @nn.compact
-    def __call__(self, carry: SkipHiddenState, key: jax.random.PRNGKey, x: jnp.ndarray):
-        """Applies the module."""
-        state = carry
-        ins, resets = x
-        reset_hs = self.initialize_carry(ins.shape[0], self.hidden_size)
-        state = jax.tree.map(lambda reset_hs, hs: jnp.where(
-            resets[:, jnp.newaxis],
-            reset_hs,
-            hs,
-        ), reset_hs, state)
-
-        # binarize in the actor loop. Need to do the u prediction here.
-        # Write a small test for this
-        u_bar = state.u_bar
-        u = jax.random.bernoulli(key, p=u_bar).astype(ins.dtype)  # (B, 1) in {0,1}
-
-        new_hidden_state, embedding = nn.GRUCell(features=self.hidden_size)(state.hidden_state, ins)
-        new_hidden_state = u * state.hidden_state + (1 - u) * new_hidden_state
-
-        # The next part is taken from the SkipRNN paper.
-        # TODO: do alternative versions of this? maybe we do want our u to be recurrent.
-        change_in_u_logit = nn.Dense(features=1)(new_hidden_state)
-        change_in_u = jax.nn.sigmoid(change_in_u_logit)
-        next_ubar = u * change_in_u + (1 - u) * (u_bar + jnp.minimum(change_in_u_logit, 1 - u_bar))
-
-        logits = nn.Dense(features=self.out_size)(embedding)
-
-        # TODO: change this for general predictions
-        out = nn.sigmoid(logits)
-
-        return SkipHiddenState(hidden_state=new_hidden_state, u_bar=next_ubar), (out, logits, u, u_bar)
-
-    @staticmethod
-    def initialize_carry(batch_size, hidden_size) -> SkipHiddenState:
-        new_hs = nn.GRUCell(features=hidden_size).initialize_carry(
-            jax.random.PRNGKey(0), (batch_size, hidden_size)
-        ).astype(float)
-        new_u = jnp.zeros((batch_size, 1)).astype(float)
-        return SkipHiddenState(new_hs, new_u)
-
-
-def train_step(step_state, _, sampler):
+def train_step(step_state, _, sampler, binarize_type: str = 'round'):
     state, rng = step_state
     sampler_rng, rng = jax.random.split(rng)
     samples = sampler(rng)
@@ -115,15 +56,21 @@ def train_step(step_state, _, sampler):
         loss_per_ex = (y - y_logits) ** 2
         # # binary cross entropy
         # loss_per_ex = optax.losses.sigmoid_binary_cross_entropy(y_logits, y)
-
-        logp = jnp.log(u_bars + 1e-10)  # (B,)
-
-        g = jax.lax.stop_gradient(loss_per_ex)
-        g_grad_log_pi = (g * (-logp)).mean()
-
         pathwise_loss = loss_per_ex.mean()
 
-        total_loss = pathwise_loss + g_grad_log_pi
+        if binarize_type == 'bernoulli':
+            # REINFORCE estimator
+            logp = jnp.log(u_bars + 1e-10)  # (B,)
+
+            g = jax.lax.stop_gradient(loss_per_ex)
+            g_grad_log_pi = (g * (-logp)).mean()
+
+            total_loss = pathwise_loss + g_grad_log_pi
+        elif binarize_type == 'round':
+            total_loss = pathwise_loss
+            g_grad_log_pi = 0.
+        else:
+            raise NotImplementedError
 
         aux = {
             'y_pred': y_preds,
@@ -148,9 +95,10 @@ def train_step(step_state, _, sampler):
 
 def train(state: TrainState, rng: jax.random.PRNGKey,
           steps: int = int(1e5), log_every: int = 50,
-          sampler: Callable = make_pulse_count_batch):
+          sampler: Callable = make_pulse_count_batch,
+          binarize_type: str = 'round'):
     epochs = steps // log_every
-    _train_step = partial(train_step, sampler=sampler)
+    _train_step = partial(train_step, sampler=sampler, binarize_type=binarize_type)
 
     def _epoch(epoch_state, i):
 
@@ -179,8 +127,9 @@ if __name__ == "__main__":
     d_in = 1
     d_hidden = 64
     t = 32
-    lr = 1e-5
+    lr = 1e-4
     steps = 100000
+    binarize_type = 'round'
 
     p = 0.1
     pulse_sampler = partial(make_pulse_count_batch, T=t, batch_size=b_size, p=p)
@@ -188,16 +137,7 @@ if __name__ == "__main__":
     sample_dict = pulse_sampler(jax.random.PRNGKey(0))
     x_sample = sample_dict['x']
 
-
-    # output one at next step, every n steps
-    # one_every = 8
-    # x = jnp.zeros((t, b_size, d_in))
-    # x = x.at[::one_every, :, 0].set(1)
-    #
-    # y = jnp.zeros((t, b_size))
-    # y = y.at[1::one_every, :].set(1)
-
-    _train = jax.jit(partial(train, steps=steps, log_every=100))
+    _train = jax.jit(partial(train, steps=steps, log_every=2, binarize_type=binarize_type))
 
     tx = optax.adam(lr)
     init_x = (
@@ -205,7 +145,7 @@ if __name__ == "__main__":
         jnp.zeros((1, b_size)),
     )
 
-    network = SkipRNN(hidden_size=d_hidden, out_size=1)
+    network = SkipRNN(hidden_size=d_hidden, out_size=1, binarize_type=binarize_type)
     init_hstate = SkipRNN.initialize_carry(b_size, d_hidden)
     rng, init_rng, call_rng = jax.random.split(rng, 3)
     call_rngs = jax.random.split(call_rng, (1, ))
