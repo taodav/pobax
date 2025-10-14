2e110eb898b04ac2540d422d14b6aef454c903cc
Ruo Yu Tao <ruoyutao@gmail.com>
2025-10-14 16:40:35 -0400

    update hparam sweeps for annas maze perfect mem

--- scripts/hyperparams/annas_maze/annas_maze_ppo_perfect_memory.py
+++ scripts/hyperparams/annas_maze/annas_maze_ppo_perfect_memory.py
@@ -2,8 +2,8 @@ from pathlib import Path
 
 exp_name = Path(__file__).stem
 
-lrs = [2.5e-3, 2.5e-4, 2.5e-5, 2.5e-6]
-lambda0s = [0.1, 0.5, 0.7, 0.9, 0.95]
+lrs = [2.5e-3, 2.5e-4, 2.5e-5]
+lambda0s = [0.5, 0.7, 0.9, 0.95]
 lambda1s = [0.5]
 ld_weights = [0]
 
@@ -26,11 +26,11 @@ hparams = {
             'entropy_coeff': 0.02,
             'num_steps': 128,
             'num_envs': 64,
-            'steps_log_freq': 16,
-            'update_log_freq': 10,
+            'steps_log_freq': 32,
+            'update_log_freq': 20,
             'total_steps': int(5e6),
-            'seed': 2025,
-            'n_seeds': 10,
+            'seed': [i + 2025 for i in range(5)],
+            'n_seeds': 2,
             'platform': 'gpu',
             'study_name': exp_name
         }
